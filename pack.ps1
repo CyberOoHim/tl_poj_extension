bf6cb3ae2303a30b95622b819abccc6ff0868e2f
CyberOoHim <inno.formosa@gmail.com>
2026-01-11 19:59:39 +0800

    feat: add datetime in the packed .zip file name

--- pack.ps1
+++ pack.ps1
@@ -1,11 +1,14 @@
 # TL -> POJ Extension Packing Script
 # Creates a clean 'extension.zip' ready for Chrome Web Store upload
 
-$zipName = "packed/extension.zip"
+$timestamp = Get-Date -Format "yyyyMMdd_HHmm"
+$zipName = "packed/extension_$timestamp.zip"
 $tempDir = "dist_temp"
+$stableLink = "packed/extension.zip"
 
 # 1. Clean up previous build
-if (Test-Path $zipName) { Remove-Item $zipName }
+# We don't delete old timestamped zips to keep history, but we clean the stable link
+if (Test-Path $stableLink) { Remove-Item $stableLink }
 if (Test-Path $tempDir) { Remove-Item $tempDir -Recurse -Force }
 
 # 2. Create temp directory
@@ -66,5 +69,9 @@ Compress-Archive -Path "$tempDir/*" -DestinationPath $zipName
 # 7. Cleanup Temp
 Remove-Item $tempDir -Recurse -Force
 
+# 8. Create Stable Link Copy
+Copy-Item $zipName -Destination $stableLink
+
 Write-Host "Done! Created $zipName" -ForegroundColor Green
+Write-Host "Also updated stable link: $stableLink" -ForegroundColor Gray
 Write-Host "You can now upload $zipName to the Chrome Web Store." -ForegroundColor Gray
